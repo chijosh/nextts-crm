2fa15687d3f26cc515203f0d26ae2aa45b70b933
Joshua Onyenwere <joshua.onyenwere@bike24.net>
2024-05-19 16:13:26 +0200

    feat(NEXTCRM-0008): add users component

---
+++ app/lib/actions.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { User } from "./models";
+import { connectToDB } from "./utils";
+
+export const deleteUser = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+    await User.findByIdAndDelete(id);
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to delete user!");
+  }
+
+  revalidatePath("/dashboard/products");
+};
+
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,25 @@
-// DUMMY DATA
+import { connectToDB } from "./utils";
+import { User } from "./models";
 
+export const fetchUsers = async (q, page) => {
+  const regex = new RegExp(q, "i");
+
+  const ITEM_PER_PAGE = 2;
+
+  try {
+    connectToDB();
+    const count = await User.find({ username: { $regex: regex } }).count();
+    const users = await User.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, users };
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch users!");
+  }
+};
+
+// DUMMY DATA
 import { ICard } from "../types/cards.interface";
 
 export const cards: ICard[] = [
