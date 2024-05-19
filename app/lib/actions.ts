91f5a50c184ba44cb643102c26a2c0ba5ee086cb
Joshua Onyenwere <joshua.onyenwere@bike24.net>
2024-05-19 16:49:50 +0200

    feat(NEXTCRM-0011): create add user page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,36 @@ import { revalidatePath } from "next/cache";
 import { User } from "./models";
 import { connectToDB } from "./utils";
 
+export const addUser = async (formData) => {
+  const { username, email, password, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+      isAdmin,
+      isActive,
+    });
+
+    await newUser.save();
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to create user!");
+  }
+
+  revalidatePath("/dashboard/users");
+  redirect("/dashboard/users");
+};
+
 export const deleteUser = async (formData) => {
   const { id } = Object.fromEntries(formData);
 
